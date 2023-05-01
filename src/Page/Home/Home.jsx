cfb59ebbb0931470b81f3cf0a89cfed29bbc427c
himaloyash <himaloyashis98@gmail.com>
2023-05-01 10:48:33 +0600

    I'm starting

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import bg1 from '/images/Rectangle 1.png'
+
+const Home = () => {
+    return (
+        <div className='img'>
+            <h2>Ok</h2>
+        </div>
+    );
+};
+
+export default Home;
