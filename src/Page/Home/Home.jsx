850c20584cda4512e8d035c42dd951e248cafa59
himaloyash <himaloyashis98@gmail.com>
2023-05-01 14:31:05 +0600

    card added

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         .catch(error=>console.log(error.message))
     },[])
     return (
-        <div className='img p-4'>
+        <div className='img p-4 '>
             <nav >
                 <div className="max-w-screen-xl px-4 py-3 mx-auto">
                     <div className="flex items-center justify-evenly">
@@ -43,14 +43,14 @@ const Home = () => {
                     </div>
                 </div>
             </nav>
-            <section>
+            <section className='flex'>
                 <div>
                     <h1 className='text-6xl text-slate-50 font-extrabold'>Cox's bazar</h1>
                     <p className='mt-2 text-slate-50'>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
                     <button type="button"  className="bg-amber-400 text-slate-100 p-2 rounded"><Link to='/booking'>BookingNow<FaArrowRight className='flex-nowrap'></FaArrowRight></Link> </button>
                     
                 </div>
-                <div>
+                <div className='flex'>
                     {
                         cards.map(card=><Card key={card.id} card={card}></Card>)
                     }
