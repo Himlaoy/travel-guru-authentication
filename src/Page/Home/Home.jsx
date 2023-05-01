ce767da8da70bd76056ef8ce716eddf00db3b54b
himaloyash <himaloyashis98@gmail.com>
2023-05-01 11:25:09 +0600

    nav added

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './Home.css';
-import bg1 from '/images/Rectangle 1.png'
+import logo from '/images/logo.png'
 
 const Home = () => {
     return (
         <div className='img'>
-            <h2>Ok</h2>
+            <nav >
+                <div class="max-w-screen-xl px-4 py-3 mx-auto">
+                    <div class="flex items-center">
+                        <img className='w-44 text-w' src={logo} alt="Travel guru" />
+                        <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                            <li>
+                                <a href="#" class=" text-white hover:underline" aria-current="page">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" class=" text-white hover:underline">Company</a>
+                            </li>
+                            <li>
+                                <a href="#" class=" text-white hover:underline">Team</a>
+                            </li>
+                            <li>
+                                <a href="#" class=" text-white hover:underline">Features</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
         </div>
     );
 };
